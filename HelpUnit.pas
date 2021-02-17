2ccdd5365a8ae210960a9d619fbcef5a0f069cae
SergeyKryloff <54943656+SergeyKryloff@users.noreply.github.com>
2021-02-17 21:52:16 +0200

    Add files via upload

--- HelpUnit.pas
+++ HelpUnit.pas
@@ -2,7 +2,7 @@
 {                                                                      }
 { Developed by Sergey A. Kryloff under the GNU General Public License. }
 {                                                                      }
-{ Software distributed under the License is distributed on an          }
+{ Software distributed under the License is provided on an             }
 { "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either expressed or     }
 { implied. See the License for the specific language governing         }
 { rights and limitations under the License.                            }
@@ -14,48 +14,26 @@
 
 unit HelpUnit;
 
+{$MODE Delphi}
+
 interface
 
-uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, OleCtrls, SHDocVw, StdCtrls;
-
-type
-  THelpForm = class(TForm)
-    WebBrowser: TWebBrowser;
-    CancelButton: TButton;
-    procedure CancelButtonClick(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
-  private
-    { Private declarations }
-    procedure CreateParams(var Params: TCreateParams); override;
-  public
-    { Public declarations }
-  end;
-
-var
-  HelpForm: THelpForm;
+procedure ShowHelp(const URL : widestring);
 
 implementation
 
-uses ActiveX;
+uses Windows, SysUtils, ActiveX, ComObj, Forms;
 
-{$R *.dfm}
-
-procedure THelpForm.CancelButtonClick(Sender: TObject);
+procedure ShowHelp(const URL : widestring);
+var InternetExplorer : OLEVariant;
 begin
- Close()
-end;
-
-procedure THelpForm.FormCreate(Sender: TObject);
-begin
- CancelButton.Width := 0; CancelButton.Height := 0
-end;
-
-procedure THelpForm.CreateParams(var Params: TCreateParams);
-begin
- inherited CreateParams(Params);
- Params.ExStyle := Params.ExStyle or WS_EX_APPWINDOW;
+ try
+  InternetExplorer := CreateOleObject('InternetExplorer.Application');
+  InternetExplorer.Visible := True;
+  InternetExplorer.Navigate(URL);
+ except
+  Application.MessageBox('Kryloff Personal Password Manager version 1.1'#13#10, PChar(Application.Title), MB_OK + MB_ICONINFORMATION);
+ end;
 end;
 
 Initialization
